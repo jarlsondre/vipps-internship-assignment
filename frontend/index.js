deb54e934c97f6a97784340c441f163e002a6390
Jarl Sondre Sæther <jssaethe@stud.ntnu.no>
2021-10-31 11:56:03 +0100

    add more comments

--- frontend/index.js
+++ frontend/index.js
@@ -4,11 +4,13 @@ async function getTopicOccurences() {
   const response = await fetch("http://localhost:3000/?topic=" + topic);
   const jsonResponse = await response.json();
   if (response.status >= 400) {
+    // Displaying an error message if something goes wrong
     const errorElement = document.getElementById("errorMessage");
     errorElement.innerHTML = jsonResponse.error;
     occurenceElement.innerHTML = "";
     return;
   } else {
+    // Removing the error message if everything goes right
     const errorElement = document.getElementById("errorMessage");
     errorElement.innerHTML = "";
   }
